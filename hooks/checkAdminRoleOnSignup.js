d940f0cfd457e86a99999ebeba3464e7905c3423
mrx7355608 <mrx7355608@gmail.com>
2024-09-22 02:42:39 +0500

    fixed validateInput hook function

--- hooks/checkAdminRoleOnSignup.js
+++ hooks/checkAdminRoleOnSignup.js
@@ -1,10 +1,9 @@
-module.exports = async ({ resolvedData, addFieldValidationError, context }) => {
-    const { authentication: { item: user } = {} } = context;
-
-    if (
-        resolvedData.role.toLowerCase() === "admin" &&
-        (!user || user.role.toLowerCase() !== "admin")
-    ) {
+module.exports = ({ resolvedData, addFieldValidationError, context }) => {
+    const { authedItem: user } = context;
+    if (resolvedData.role.toLowerCase() === "admin") {
+        if (user && user.role.toLowerCase() === "admin") {
+            return true;
+        }
         addFieldValidationError("You cannot assign the 'admin' role");
     }
 };
