5075a0c9d557b2db607ee3c53adcce8707505d72
mrx7355608 <mrx7355608@gmail.com>
2024-09-22 02:11:03 +0500

    updated user schema

--- lists/user.js
+++ lists/user.js
@@ -1,29 +1,44 @@
-const { Text, Password, Checkbox } = require("@keystonejs/fields");
+const { Text, Password, Select } = require("@keystonejs/fields");
 const access = require("../utils/accessControl");
+const checkAdminRoleOnSignup = require("../hooks/checkAdminRoleOnSignup");
 
-module.exports = {
+const User = {
     fields: {
-        name: { type: Text },
-        email: {
+        name: { type: Text, isRequired: true },
+        email: { type: Text, isRequired: true, isUnique: true },
+        password: { type: Password, isRequired: true },
+        profile_picture: {
             type: Text,
-            isUnique: true,
+            defaultValue: process.env.DEFAULT_PROFILE_PICTURE,
         },
-        isAdmin: {
-            type: Checkbox,
+        role: {
+            type: Select,
+            options: "admin, exhibitor, attendee",
+            defaultValue: "attendee",
+            dataType: "string",
+            isRequired: true,
             access: {
                 update: access.userIsAdmin,
             },
-        },
-        password: {
-            type: Password,
+            hooks: {
+                validateInput: checkAdminRoleOnSignup,
+            },
         },
     },
-    // List-level access controls
+
+    adminConfig: {
+        defaultColumns: "name, email, role",
+        defaultSort: "name",
+    },
     access: {
         read: access.userIsAdminOrOwner,
         update: access.userIsAdminOrOwner,
-        create: access.userIsAdmin,
+        // create: access.userIsAdmin,
         delete: access.userIsAdmin,
         auth: true,
     },
 };
+
+module.exports = User;
+
+// List-level access controls
