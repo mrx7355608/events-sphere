490b0b758af87fdd37d82f70ec291d955db33bec
mrx7355608 <mrx7355608@gmail.com>
2024-09-23 17:00:26 +0500

    added a cookieSecret (it's still not working lol)

--- keystone.js
+++ keystone.js
@@ -5,6 +5,7 @@ const mongoAdapter = require("./utils/db");
 const keystone = new Keystone({
     adapter: mongoAdapter,
     onConnect: process.env.CREATE_TABLES !== "true" && initialiseData,
+    cookieSecret: process.env.COOKIE_SECRET,
 });
 
 module.exports = keystone;
