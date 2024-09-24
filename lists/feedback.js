5e39499b52ef7a1fdddc41d285a19d626533abdd
mrx7355608 <mrx7355608@gmail.com>
2024-09-24 22:13:52 +0500

    added access-control to allow valid feedbacks
    I have added an access-control on create operation that prevents
    feedbacks from users who have not attended the event as an attendee or
    as an exhibitor, only those can provide feedback who have attended the
    event

--- lists/feedback.js
+++ lists/feedback.js
@@ -1,5 +1,9 @@
 const { Text, Relationship, Select } = require("@keystonejs/fields");
 const accessControl = require("../utils/accessControl");
+const {
+    AuthedRelationship,
+} = require("@keystonejs/fields-authed-relationship/dist/fields-authed-relationship.cjs.prod");
+const keystone = require("../keystone");
 
 // Access control function for update and delete operations
 const userIsAdminOrOwner = ({ existingItem, authentication }) => {
@@ -18,6 +22,44 @@ const userIsAdminOrOwner = ({ existingItem, authentication }) => {
     return false;
 };
 
+const hasUserAttendedEvent = async (userId, userRole, eventId, context) => {
+    const query = `
+        query HasAttendedEvent ($eventId: ID!) {
+            Event (where: { id: $eventId }) {
+                attendees {
+                    id,
+                },
+                exhibitors {
+                    id,
+                },
+            }
+        }
+    `;
+
+    // 1. Make graphql query
+    const result = await keystone.executeGraphQL({
+        query,
+        variables: { eventId },
+        context,
+    });
+
+    // 2. Throw error if any
+    if (result.errors) {
+        console.log(result.errors);
+        throw new Error("Unable to fetch event details");
+    }
+
+    // 3. Create an array of IDs from the result.data
+    const { attendees, exhibitors } = result.data.Event;
+    const userIDs =
+        userRole === "attendee"
+            ? attendees.map((obj) => obj.id)
+            : exhibitors.map((obj) => obj.id);
+
+    // 4. Return true/false boolean
+    return userIDs.includes(userId);
+};
+
 const Feedback = {
     fields: {
         feedback: {
@@ -32,7 +74,7 @@ const Feedback = {
             defaultValue: "Good",
         },
         user: {
-            type: Relationship,
+            type: AuthedRelationship,
             isRequired: true,
             many: false,
             ref: "User",
@@ -47,7 +89,33 @@ const Feedback = {
 
     access: {
         read: accessControl.isSignedIn,
-        create: accessControl.isSignedIn,
+        create: async ({ authentication, originalInput, context }) => {
+            const user = authentication.item;
+            if (!user) {
+                return false;
+            }
+
+            if (user.role !== "admin") {
+                // Check if user has attended the event
+                const hasAttendedEvent = await hasUserAttendedEvent(
+                    user.id.toString(),
+                    user.role,
+                    originalInput.event.connect.id.toString(),
+                    context
+                );
+
+                // If not, throw error
+                if (!hasAttendedEvent) {
+                    throw new Error(
+                        "You cannot give feedback to event you have not attended"
+                    );
+                }
+
+                return true;
+            }
+
+            return true;
+        },
         update: userIsAdminOrOwner,
         delete: userIsAdminOrOwner,
     },
